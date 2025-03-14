5111f8c78c087fb803261b80e81e4125e8b70a09
sjonynaite <s.jonynaite@ba.lt>
2025-03-14 14:18:34 +0200

    added more prebuilds for different node versions

--- bindings/node/index.js
+++ bindings/node/index.js
@@ -1,19 +1,52 @@
-try {
-  module.exports = require("../../build/Release/tree_sitter_abl_binding");
-} catch (error1) {
-  if (error1.code !== 'MODULE_NOT_FOUND') {
-    throw error1;
-  }
+const path = require("path");
+const os = require("os");
+const { versions } = process;
+
+function loadModule() {
   try {
-    module.exports = require("../../build/Debug/tree_sitter_abl_binding");
-  } catch (error2) {
-    if (error2.code !== 'MODULE_NOT_FOUND') {
-      throw error2;
+    // Try loading prebuilt binary dynamically based on Node.js version
+    const modulePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "prebuilds",
+      "win32-x64",
+      `yourmodule-${versions.modules}.node`
+    );
+
+    console.log(`Loading prebuilt binary from: ${modulePath}`);
+    return require(modulePath);
+  } catch (error1) {
+    if (error1.code !== "MODULE_NOT_FOUND") {
+      throw error1;
     }
-    throw error1
+
+    try {
+      // Fallback to manually built Release version
+      return require("../../build/Release/tree_sitter_abl_binding");
+    } catch (error2) {
+      if (error2.code !== "MODULE_NOT_FOUND") {
+        throw error2;
+      }
+
+      try {
+        // Fallback to manually built Debug version
+        return require("../../build/Debug/tree_sitter_abl_binding");
+      } catch (error3) {
+        if (error3.code !== "MODULE_NOT_FOUND") {
+          throw error3;
+        }
+
+        throw error1; // If all attempts fail, throw the original error
+      }
+    }
   }
 }
 
+// Load the correct module
+module.exports = loadModule();
+
+// Try loading node type info if available
 try {
   module.exports.nodeTypeInfo = require("../../src/node-types.json");
 } catch (_) {}
