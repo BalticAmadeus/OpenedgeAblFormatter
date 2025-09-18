ddfa2fb7493494fa24573b2705e3a0a4fd12a285
Gytis Raginskas <g.raginskas@ba.lt>
2025-09-18 15:58:28 +0300

    first commit

--- src/extension.ts
+++ src/extension.ts
@@ -38,6 +38,31 @@ export async function activate(context: vscode.ExtensionContext) {
     const hoverProvider = new AblDebugHoverProvider(parserHelper);
     vscode.languages.registerHoverProvider(Constants.ablId, hoverProvider);
     Telemetry.sendExtensionSettings();
+
+    // Register 'Report a Bug' command
+    const reportBugCommand = vscode.commands.registerCommand(
+        "openedgeAblFormatter.reportBug",
+        () => {
+            vscode.env.openExternal(
+                vscode.Uri.parse(
+                    "https://github.com/BalticAmadeus/AblFormatter/issues/new/choose"
+                )
+            );
+        }
+    );
+    context.subscriptions.push(reportBugCommand);
+
+    // Add a status bar button for reporting bugs
+    const bugStatusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Right,
+        99
+    );
+    bugStatusBarItem.text = "$(bug) Report Bug";
+    bugStatusBarItem.command = "openedgeAblFormatter.reportBug";
+    bugStatusBarItem.tooltip =
+        "Report a bug or issue for OpenEdge ABL Formatter";
+    bugStatusBarItem.show();
+    context.subscriptions.push(bugStatusBarItem);
 }
 
 // This method is called when your extension is deactivated
